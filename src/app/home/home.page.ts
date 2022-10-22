c227878e61b240a171cc982bd7e917eadd42a903
Osman Jimenez <osmanjimenez.dev@gmail.com>
2022-10-22 13:45:36 -0500

    Feat: create form

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  createForm: FormGroup;
 
-  constructor() {}
+  constructor(public formBuilder: FormBuilder) {}
 
+  ngOnInit() {
+    this.validateForm();
+  }
+
+  validateForm() {
+    this.createForm = this.formBuilder.group({
+      first_name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(20),
+        ],
+      ],
+      last_name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(20),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(20),
+        ],
+      ],
+      birth_date: [''],
+      identification: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(20),
+        ],
+      ],
+      gender: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  get errorControl() {
+    return this.createForm.controls;
+  }
 }
